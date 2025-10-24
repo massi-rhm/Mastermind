658ebf92df8fa5bca9755e3ddc362a4f63a6dc65
massi-rhm <m_rahem@hetic.eu>
2025-10-24 19:20:56 +0200

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -7,4 +7,21 @@ def generate_secret_code():
     return code
 
 secret_code = generate_secret_code()
-print("code secret =", secret_code) 
+
+def get_player_guess():
+    while True:
+        guess_input = input("Entrez 4 chiffres (1-6) séparés par des espaces : ")
+        guess_str = guess_input.split()
+        if len(guess_str) != 4:
+            print("Il faut exactement 4 chiffres !")
+            continue
+        try:
+            guess = [int(x) for x in guess_str]
+        except:
+            print("Entrez uniquement des chiffres !")
+            continue
+        if any(x < 1 or x > 6 for x in guess):
+            print(" Chaque chiffre doit être entre 1 et 6 !")
+            continue
+        return guess
+    
